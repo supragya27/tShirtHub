6f6de1697de5125988353aec80ba4539a205363d
supragya27 <supragya.chauhan001@gmail.com>
2020-04-26 17:16:11 +0530

    brought app.js to the correct location and updated its content

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const express = require("express")
+const app=express();
+mongoose.connect('mongodb://localhost:27017/tshirt', {useNewUrlParser: true, useUnifiedTopology: true,
+useCreateIndex: true}).then(()=>{
+    console.log("DB CONNECTED");
+});
+
+const port = 2700;
+app.listen(port,()=>{
+    console.log(`app is running at ${port}`);
+})
