328cb67df2c43382717b74f7f0b8bdc3c64e3786
supragya27 <supragya.chauhan001@gmail.com>
2020-04-27 16:36:55 +0530

    custom middlewares

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,23 @@
 require('dotenv').config()
+
 const mongoose = require('mongoose');
 const express = require("express")
 const app=express();
+const bodyParser=require('body-parser');
+const cookieParser=require('cookie-parser');
+const cors=require('cors');
+
 mongoose.connect(process.env.DATABASE, {useNewUrlParser: true, useUnifiedTopology: true,
 useCreateIndex: true}).then(()=>{
     console.log("DB CONNECTED");
 });
 
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
 const port = process.env.PORT||2700;
+
 app.listen(port,()=>{
     console.log(`app is running at ${port}`);
 })
