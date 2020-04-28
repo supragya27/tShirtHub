c51819444d49817340404204bcf0097de88cd1a0
supragya27 <supragya.chauhan001@gmail.com>
2020-04-28 21:12:12 +0530

    added validations

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,13 @@
 var express = require('express')
 var router = express.Router()
+const { check } = require('express-validator');
 const {signout,signup} =require('../controllers/auth')
 
-router.post('/signup',signup);
+router.post('/signup',[
+    check('name').isLength({min:3}).withMessage('Name should be atleast 3 letters.'),
+    check('email').isEmail().withMessage('Invalid email'),
+    check('password').isLength({min:3}).withMessage('Password should be atleast 3 characters long.')
+],signup);
 router.get('/signout',signout);
 
 module.exports=router;
