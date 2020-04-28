7540e05d840909aa5cbf30fe2579548c3bc724fe
supragya27 <supragya.chauhan001@gmail.com>
2020-04-28 10:56:33 +0530

    authentication controllers

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,7 @@
 var express = require('express')
 var router = express.Router()
+const {signout} =require('../controllers/auth')
 
-const signout = (req,res)=>{
-    res.json({
-        message:"user has signed out"
-    })
-}
 
 router.get('/signout',signout);
 
