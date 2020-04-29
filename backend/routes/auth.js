d051644c9d20404f03af33c1a2f80d7c81105a4a
supragya27 <supragya.chauhan001@gmail.com>
2020-04-29 22:49:01 +0530

    signin route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,19 @@
 var express = require('express')
 var router = express.Router()
 const { check } = require('express-validator');
-const {signout,signup} =require('../controllers/auth')
+const {signout,signup,signin} =require('../controllers/auth')
 
 router.post('/signup',[
     check('name').isLength({min:3}).withMessage('Name should be atleast 3 letters.'),
     check('email').isEmail().withMessage('Invalid email'),
     check('password').isLength({min:3}).withMessage('Password should be atleast 3 characters long.')
 ],signup);
+
+router.post('/signin',[
+    check('email').isEmail().withMessage('Invalid email'),
+    check('password').isLength({min:1}).withMessage('Password field is compulsory.')
+],signin);
+
 router.get('/signout',signout);
 
 module.exports=router;
