a172be34e8b979a17604751c793555208da9679b
supragya27 <supragya.chauhan001@gmail.com>
2020-04-29 22:49:58 +0530

    signin controller

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,5 +1,7 @@
 const User=require('../models/user')
 const { check,validationResult } = require('express-validator');
+var jwt = require('jsonwebtoken');
+var expressJwt = require('express-jwt');
 
 exports.signup=(req,res)=>{
 
@@ -25,6 +27,42 @@ exports.signup=(req,res)=>{
        })
    })
 }
+
+exports.signin=(req,res)=>{
+    const errors=validationResult(req)
+    const {email,password} = req.body;
+   
+    if(!errors.isEmpty()){
+        return res.status(422).json({
+            error: errors.array()[0].msg 
+        })
+    }
+    User.findOne({email},(err,user)=>{
+        if(err||!user){
+        return res.status(400).json({
+            error: "email does not exist"
+        })
+    }
+    if(!user.authenticate(password)){
+        return res.status(401).json({
+            error: "invalid password"
+        })
+    }
+
+    //create token
+         const token=jwt.sign({_id:user._id},process.env.SECRET)
+    //put token in cookie
+         res.cookie("token",token,{expire: new Date()+9999})
+
+    //send response to front end
+    const {_id,name,email,role}=user;
+    return res.json({token,user:{_id,name,email,role}});
+
+        })
+
+}
+
+
 
 exports.signout = (req,res)=>{
     res.json({
