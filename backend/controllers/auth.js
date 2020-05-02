701a00932576eddb68b6123cf63e8de2816aeb8b
supragya27 <supragya.chauhan001@gmail.com>
2020-05-02 14:13:31 +0530

    isAdmin custom middleware

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -88,3 +88,11 @@ exports.isAuthenticated=(req,res,next)=>{
     next();
 } 
 
+exports.isAdmin=(req,res,next)=>{
+    if(req.profile.role===0){
+        return res.status(403).json({
+            error: "You are not ADMIN, Access denied"
+        });
+    }
+    next();
+}
