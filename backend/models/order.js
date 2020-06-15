ddfb83fb661cf92575a8b1a97ea8ff7065303412
supragya27 <supragya.chauhan001@gmail.com>
2020-06-15 09:18:50 +0530

    fixed a bug

--- backend/models/order.js
+++ backend/models/order.js
@@ -4,7 +4,7 @@ const {ObjectId}=mongoose.Schema;
 const ProductCartSchema=new mongoose.Schema({
     product:{
         type:ObjectId,
-        ref="Product"
+        ref:"Product"
     },
     name: String,
     count: Number,
