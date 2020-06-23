38c83c619eb4045f8902d4bb7e308de1e57eb76e
supragya27 <supragya.chauhan001@gmail.com>
2020-06-23 22:27:00 +0530

    updated

--- backend/controllers/category.js
+++ backend/controllers/category.js
@@ -1,6 +1,13 @@
 const Category = require("../models/category")
 
 exports.getCategoryById = (req, res, next, id) => {
-
-    next()
+    Category.findById(id).exec((err, ctg) => {
+        if (err) {
+            return res.status(400).json({
+                error: "Category not found in database"
+            })
+        }
+        req.category = ctg;
+        next();
+    })
 }
