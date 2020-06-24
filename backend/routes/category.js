890c72339d4a220df6ecb186bdb5e78fbd685bd7
supragya27 <supragya.chauhan001@gmail.com>
2020-06-24 11:28:48 +0530

    added updateCategory route and controller

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const { getCategoryById, createCategory, getCategory, getAllCategory } = require("../controllers/category")
+const { getCategoryById, createCategory, getCategory, getAllCategory, updateCategory } = require("../controllers/category")
 const { isSignedIn, isAdmin, isAuthenticated } = require("../controllers/auth")
 const { getUserById } = require("../controllers/user")
 
@@ -10,9 +10,17 @@ router.param("userId", getUserById)
 router.param("categoryId", getCategoryById)
 
 //actual routes
+
+//create
 router.post("/category/create/:userId", isSignedIn, isAuthenticated, isAdmin, createCategory)
 
+//read
 router.get("/category/:categoryId", getCategory)
 router.get("/categories", getAllCategory)
+
+//update
+router.put("/category/:categoryId", isSignedIn, isAuthenticated, isAdmin, updateCategory)
+
+//delete
 
 module.exports = router;
