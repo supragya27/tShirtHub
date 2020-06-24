868748a9a0aafcce7a7103cab0aadc69ad5c28ed
supragya27 <supragya.chauhan001@gmail.com>
2020-06-24 22:51:01 +0530

    added delete and updateProduct route and controller

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -81,3 +81,58 @@ exports.photo = (req, res, next) => {
     }
     next()
 }
+
+//delete controller
+exports.deleteProduct = (req, res) => {
+    let product = req.product
+    product.remove((err, deletedProduct) => {
+        if (err) {
+            return res.status(400).json({
+                error: "Failed to delete product"
+            })
+        }
+        res.json({
+            message: "Deleted successfully",
+            deleteProduct
+        })
+    })
+}
+
+//update controller
+exports.updateProduct = (req, res) => {
+    let form = new formidable.IncomingForm()
+    form.keepExtensions = true;
+
+    form.parse(req, (err, fields, file) => {
+        if (err) {
+            return res.status(400).json({
+                error: "problem with image"
+            })
+        }
+
+        //updation code
+        let product = req.Product
+        product = _.extend(product, fields)
+
+        //handle file
+        if (file.photo) {
+            if (file.photo.size > 3000000) {
+                return res.status(400).json({
+                    error: "File size too big!"
+                })
+            }
+            product.photo.data = fs.readFileSync(file.photo.path)
+            product.photo.contentType = file.photo.type
+        }
+
+        //save to DB
+        product.save((err, product) => {
+            if (err) {
+                res.status(400).json({
+                    error: "updation failed"
+                })
+            }
+            res.json(product)
+        })
+    })
+}
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 
-const { getProductById, createProduct, getProduct, photo } = require("../controllers/product")
+const { getProductById, updateProduct, deleteProduct, createProduct, getProduct, photo } = require("../controllers/product")
 const { isAdmin, isAuthenticated, isSignedIn } = require("../controllers/auth")
 const { getUserById } = require("../controllers/user")
 
@@ -10,8 +10,17 @@ router.param("userId", getUserById)
 router.param("productId", getProductById)
 
 //routes
+//create route
 router.post("/product/create/:userId", isSignedIn, isAuthenticated, isAdmin, createProduct)
+
+//read routes
 router.get("product/:productId", getProduct)
 router.get("product/photo/:productId", photo)
 
+//delete route
+router.delete("/product/:productId/:userId", isSignedIn, isAuthenticated, isAdmin, deleteProduct)
+//update route
+router.put("/product/:productId/:userId", isSignedIn, isAuthenticated, isAdmin, updateProduct)
+
+//listing route
 module.exports = router
