4c731d90969095f2b178abbaa9627ea4e07b0388
supragya27 <supragya.chauhan001@gmail.com>
2020-06-25 00:55:00 +0530

    added updateStock controller

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -2,6 +2,7 @@ const Product = require("../models/product")
 const formidable = require("formidable")
 const _ = require("lodash")
 const fs = require("fs")
+const { updateOne } = require("../models/product")
 
 exports.getProductById = (req, res, next, id) => {
     Product.findById(id)
@@ -157,3 +158,23 @@ exports.getAllProducts = (req, res) => {
             res.json(products)
         })
 }
+
+exports.updateStock = (req, res, next) => {
+    let myOperations = req.body.order.products.map(prod => {
+        return {
+            updateOne: {
+                filter: { _id: prod._id },
+                update: { $inc: { stock: -prod.count, sold: +prod.count } }
+            }
+        };
+    });
+
+    Product.bulkWrite(myOperations, {}, (err, products) => {
+        if (err) {
+            return res.status(400).json({
+                error: "Bulk operation failed"
+            });
+        }
+        next();
+    });
+};
