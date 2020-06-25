fda1a308976d11376b875462cb8fe21d4745d5f6
supragya27 <supragya.chauhan001@gmail.com>
2020-06-25 09:02:22 +0530

    added the createOrder route and controller

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -6,13 +6,14 @@ const { getUserById, pushOrderInPurchaseList } = require("../controllers/user")
 const { updateStock } = require("../controllers/product")
 
 
-const { getOrderById } = require("../controllers/order")
+const { getOrderById, createOrder } = require("../controllers/order")
 
 //params
 router.param("userId", getUserById)
 router.param("orderId", getOrderById)
 
 //actual routes
-
+//create
+router.post("order/create/:userId", isSignedIn, isAuthenticated, pushOrderInPurchaseList, updateStock, createOrder)
 
 module.exports = router
