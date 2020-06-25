8d11a3eeb9dba6e166b839ebea3f1d67d35cddfe
supragya27 <supragya.chauhan001@gmail.com>
2020-06-25 08:50:04 +0530

    added getOrderById route and controller

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -6,7 +6,13 @@ const { getUserById, pushOrderInPurchaseList } = require("../controllers/user")
 const { updateStock } = require("../controllers/product")
 
 
-const { } = require("../controllers/order")
+const { getOrderById } = require("../controllers/order")
+
+//params
+router.param("userId", getUserById)
+router.param("orderId", getOrderById)
+
+//actual routes
 
 
 module.exports = router
