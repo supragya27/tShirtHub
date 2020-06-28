d767fb01ad4619865fe6b9550f9ba925b1efdc9a
supragya27 <supragya.chauhan001@gmail.com>
2020-06-28 10:32:45 +0530

    updated signin button's visibility

--- frontend/src/core/Menu.js
+++ frontend/src/core/Menu.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link, withRouter } from 'react-router-dom'
+import { signout, isAuthenticated } from '../auth/helper'
 
 const currentTab = (history, path) => {
     if (history.location.pathname === path) {
@@ -43,11 +44,18 @@ const Menu = ({ history }) => (
                     Signin
                 </Link>
             </li>
-            <li className="nav-item">
-                <Link style={currentTab(history, '/signout')} className="nav-link" to="/signout">
+            {isAuthenticated() && (<li className="nav-item">
+                <span className="nav-link text-warning"
+                    onClick={() => {
+                        signout(() => {
+                            history.push('/')
+                        })
+                    }}
+                >
                     Signout
-                </Link>
-            </li>
+                </span>
+            </li>)}
+
         </ul>
     </div>
 )
