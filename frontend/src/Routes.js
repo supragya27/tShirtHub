fb6328890eb842f6d0440c3ff2d0bd3baff93718
supragya27 <supragya.chauhan001@gmail.com>
2020-06-29 14:22:36 +0530

    added createCategory route

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,21 +1,41 @@
-import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import Home from './core/Home'
-import Signup from './user/Signup'
-import Signin from './user/Signin'
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./core/Home";
+import Signup from "./user/Signup";
+import Signin from "./user/Signin";
+import AdminRoute from "./auth/helper/AdminRoutes";
+import PrivateRoute from "./auth/helper/PrivateRoutes";
+import UserDashBoard from "./user/UserDashBoard";
+import AdminDashBoard from "./user/AdminDashBoard";
+import AddCategory from "./admin/AddCategory";
 
 function Routes() {
-    return (
-        <div>
-            <BrowserRouter>
-                <Switch>
-                    <Route path='/' exact component={Home} />
-                    <Route path='/signup' exact component={Signup} />
-                    <Route path='/signin' exact component={Signin} />
-                </Switch>
-            </BrowserRouter>
-        </div>
-    )
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/signup" exact component={Signup} />
+          <Route path="/signin" exact component={Signin} />
+          <PrivateRoute
+            path="/user/dashboard"
+            exact
+            component={UserDashBoard}
+          />
+          <AdminRoute
+            path="/admin/dashboard"
+            exact
+            component={AdminDashBoard}
+          />
+          <AdminRoute
+            path="/admin/create/category"
+            exact
+            component={AddCategory}
+          />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default Routes
+export default Routes;
