5e9ca23c82289a1a5080d4e8cbec0ebd831b0f21
supragya27 <supragya.chauhan001@gmail.com>
2020-06-29 10:06:53 +0530

    updated with user and admin dashboard

---
+++ frontend/src/user/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Base from '../core/Base'
+
+function Profile() {
+    return (
+        <Base title="Profile page">
+            <h1>this is profile page</h1>
+        </Base>
+    )
+}
+
+export default Profile
