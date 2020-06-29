cf59fd8bb8436f8955d5189f61dab5fea9950718
supragya27 <supragya.chauhan001@gmail.com>
2020-06-29 10:06:19 +0530

    updated

---
+++ frontend/src/user/AdminDashBoard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Base from '../core/Base'
+
+function AdminDashBoard() {
+    return (
+        <Base title="AdminDashBoard page">
+            <h1>this is AdminDashBoard page</h1>
+        </Base>
+    )
+}
+
+export default AdminDashBoard
