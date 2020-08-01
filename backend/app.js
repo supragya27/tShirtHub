31d97b330e8632d25709a8173220657d160fe71d
supragya27 <supragya.chauhan001@gmail.com>
2020-08-01 23:11:45 +0530

    updated with stripe checkout code

--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,30 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const mongoose = require('mongoose');
-const express = require("express")
+const mongoose = require("mongoose");
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 //My routes
-const authRoutes = require('./routes/auth')
-const userRoutes = require('./routes/user')
-const categoryRoutes = require('./routes/category')
-const productRoutes = require('./routes/product')
-const orderRoutes = require("./routes/order")
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+const orderRoutes = require("./routes/order");
+const stripeRoutes = require("./routes/stripepayment");
 
 //DB Connection
-mongoose.connect(process.env.DATABASE, {
-    useNewUrlParser: true, useUnifiedTopology: true,
-    useCreateIndex: true
-}).then(() => {
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
     console.log("DB CONNECTED");
-});
-
+  });
 
 //middlewares
 app.use(bodyParser.json());
@@ -29,16 +32,17 @@ app.use(cookieParser());
 app.use(cors());
 
 //My Routes
-app.use('/api', authRoutes)
-app.use('/api', userRoutes)
-app.use('/api', categoryRoutes)
-app.use('/api', productRoutes)
-app.use('/api', orderRoutes)
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+app.use("/api", orderRoutes);
+app.use("/api", stripeRoutes);
 
 //port
 const port = process.env.PORT || 2700;
 
 //starting a server
 app.listen(port, () => {
-    console.log(`app is running at ${port}`);
-})
+  console.log(`app is running at ${port}`);
+});
