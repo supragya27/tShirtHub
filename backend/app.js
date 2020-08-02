3a85a2bcf861d6005040d952ab702d6a2a8a672f
supragya27 <supragya.chauhan001@gmail.com>
2020-08-02 11:33:40 +0530

    braintree integration updates

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,7 @@ const categoryRoutes = require("./routes/category");
 const productRoutes = require("./routes/product");
 const orderRoutes = require("./routes/order");
 const stripeRoutes = require("./routes/stripepayment");
+const paymentBRoutes = require("./routes/bpayment");
 
 //DB Connection
 mongoose
@@ -38,6 +39,7 @@ app.use("/api", categoryRoutes);
 app.use("/api", productRoutes);
 app.use("/api", orderRoutes);
 app.use("/api", stripeRoutes);
+app.use("/api", paymentBRoutes);
 
 //port
 const port = process.env.PORT || 2700;
