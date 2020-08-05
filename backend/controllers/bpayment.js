b4006462689910fa854b28cf955be836bee10311
supragya27 <supragya.chauhan001@gmail.com>
2020-08-05 21:34:31 +0530

    updated

--- backend/controllers/bpayment.js
+++ backend/controllers/bpayment.js
@@ -2,9 +2,9 @@ var braintree = require("braintree");
 
 var gateway = braintree.connect({
   environment: braintree.Environment.Sandbox,
-  merchantId: "wx33cpy36r3gxpnr",
-  publicKey: "rhmccxcbbwhmtv9h",
-  privateKey: "c95bbc723884a6dba1093cb548dfb435",
+  merchantId: process.env.MID,
+  publicKey: process.env.PK,
+  privateKey: process.env.PVTK,
 });
 
 exports.getToken = (req, res) => {
